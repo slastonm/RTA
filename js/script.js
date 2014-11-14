04df27c718006580b23c647287e86e89bce10eac
slastonm <slastonm@gmail.com>
2014-11-14 17:01:02 +0200

    js

---
+++ js/script.js
@@ -0,0 +1,217 @@
+$(document).ready(function() { 
+    $('a#go').click( function(event){ 
+        event.preventDefault(); 
+        $('#overlay').fadeIn(400, 
+            function(){ 
+                $('#modal_form') 
+                    .css('display', 'block') // 
+                    .animate({opacity: 1, top: '10%'}, 100); // плавно прибавляем прозрачность одновременно со съезжанием вниз
+        });
+    });
+    /* Закрытие модального окна, тут делаем то же самое но в обратном порядке */
+    $('#modal_close, #overlay,.send').click( function(){ // ловим клик по крестику или подложке
+        $('#modal_form')
+            .animate({opacity: 0, top: '45%'}, 100,  // плавно меняем прозрачность на 0 и одновременно двигаем окно вверх
+                function(){ // после анимации
+                    $(this).css('display', 'none'); // делаем ему display: none;
+                    $('#overlay').fadeOut(300); // скрываем подложку
+                }
+            );
+    });
+	
+	alignCenter($('#modal_form'));
+	function alignCenter(elem) {
+    elem.css({
+      left: ($(window).width() - elem.width()) / 2 + 'px', // получаем координату центра по ширине
+      top: ($(window).height() - elem.height()) / 2 + 'px' // получаем координату центра по высоте
+    })
+  }
+});
+
+
+
+$(document).ready(function() { 
+    $('a#go2').click( function(event){ 
+        event.preventDefault(); 
+        $('#overlay').fadeIn(400, 
+            function(){ 
+                $('#modal_form_regist') 
+                    .css('display', 'block') // 
+                    .animate({opacity: 1, top: '10%'}, 100); // плавно прибавляем прозрачность одновременно со съезжанием вниз
+        });
+    });
+    /* Закрытие модального окна, тут делаем то же самое но в обратном порядке */
+    $('#modal_close, #overlay,.send').click( function(){ // ловим клик по крестику или подложке
+        $('#modal_form_regist')
+            .animate({opacity: 0, top: '45%'}, 100,  // плавно меняем прозрачность на 0 и одновременно двигаем окно вверх
+                function(){ // после анимации
+                    $(this).css('display', 'none'); // делаем ему display: none;
+                    $('#overlay').fadeOut(300); // скрываем подложку
+                }
+            );
+    });
+    
+    alignCenter($('#modal_form_regist'));
+    function alignCenter(elem) {
+    elem.css({
+      left: ($(window).width() - elem.width()) / 2 + 'px', // получаем координату центра по ширине
+      top: ($(window).height() - elem.height()) / 2 + 'px' // получаем координату центра по высоте
+    })
+  }
+});
+
+
+$(document).ready(function() { 
+    $('a#go3').click( function(event){ 
+        event.preventDefault(); 
+        $('#overlay').fadeIn(400, 
+            function(){ 
+                $('#modal_form_enter') 
+                    .css('display', 'block') // 
+                    .animate({opacity: 1, top: '10%'}, 100); // плавно прибавляем прозрачность одновременно со съезжанием вниз
+        });
+    });
+    /* Закрытие модального окна, тут делаем то же самое но в обратном порядке */
+    $('#modal_close, #overlay,.send').click( function(){ // ловим клик по крестику или подложке
+        $('#modal_form_enter')
+            .animate({opacity: 0, top: '45%'}, 100,  // плавно меняем прозрачность на 0 и одновременно двигаем окно вверх
+                function(){ // после анимации
+                    $(this).css('display', 'none'); // делаем ему display: none;
+                    $('#overlay').fadeOut(300); // скрываем подложку
+                }
+            );
+    });
+    
+    alignCenter($('#modal_form_enter'));
+    function alignCenter(elem) {
+    elem.css({
+      left: ($(window).width() - elem.width()) / 2 + 'px', // получаем координату центра по ширине
+      top: ($(window).height() - elem.height()) / 2 + 'px' // получаем координату центра по высоте
+    })
+  }
+});
+
+$(document).ready(function() { 
+    $('a#go6').click( function(event){ 
+        event.preventDefault(); 
+        $('#overlay').fadeIn(400, 
+            function(){ 
+                $('#modal_form_recov') 
+                    .css('display', 'block') // 
+                    .animate({opacity: 1, top: '10%'}, 100); // плавно прибавляем прозрачность одновременно со съезжанием вниз
+        });
+    });
+    /* Закрытие модального окна, тут делаем то же самое но в обратном порядке */
+    $('#modal_close, #overlay,.send').click( function(){ // ловим клик по крестику или подложке
+        $('#modal_form_recov')
+            .animate({opacity: 0, top: '45%'}, 100,  // плавно меняем прозрачность на 0 и одновременно двигаем окно вверх
+                function(){ // после анимации
+                    $(this).css('display', 'none'); // делаем ему display: none;
+                    $('#overlay').fadeOut(300); // скрываем подложку
+                }
+            );
+    });
+    
+    alignCenter($('#modal_form_recov'));
+    function alignCenter(elem) {
+    elem.css({
+      left: ($(window).width() - elem.width()) / 2 + 'px', // получаем координату центра по ширине
+      top: ($(window).height() - elem.height()) / 2 + 'px' // получаем координату центра по высоте
+    })
+  }
+});
+
+
+$(document).ready(function() { 
+    $('#back').click( function(event){ 
+        event.preventDefault(); 
+        $('#overlay').fadeIn(400, 
+            function(){ 
+                $('#modal_form_enter') 
+                    .css('display', 'block') // 
+                    .animate({opacity: 1, top: '10%'}, 100); // плавно прибавляем прозрачность одновременно со съезжанием вниз
+        });
+    });
+    /* Закрытие модального окна, тут делаем то же самое но в обратном порядке */
+    $('#modal_close, #overlay,.send, #back').click( function(){ // ловим клик по крестику или подложке
+        $('#modal_form_recov')
+            .animate({opacity: 0, top: '45%'}, 100,  // плавно меняем прозрачность на 0 и одновременно двигаем окно вверх
+                function(){ // после анимации
+                    $(this).css('display', 'none'); // делаем ему display: none;
+                    $('#modal_form_recov').fadeOut(300); // скрываем подложку
+                }
+            );
+    });
+    
+    alignCenter($('#modal_form_enter'));
+    function alignCenter(elem) {
+    elem.css({
+      left: ($(window).width() - elem.width()) / 2 + 'px', // получаем координату центра по ширине
+      top: ($(window).height() - elem.height()) / 2 + 'px' // получаем координату центра по высоте
+    })
+  }
+});
+
+
+// to registration
+$(document).ready(function() { 
+    $('a#to_reg').click( function(event){ 
+        event.preventDefault(); 
+        $('#overlay').fadeIn(400, 
+            function(){ 
+                $('#modal_form_regist') 
+                    .css('display', 'block') // 
+                    .animate({opacity: 1, top: '10%'}, 100); // плавно прибавляем прозрачность одновременно со съезжанием вниз
+        });
+    });
+    /* Закрытие модального окна, тут делаем то же самое но в обратном порядке */
+    $('#modal_close, #overlay,.send').click( function(){ // ловим клик по крестику или подложке
+        $('#modal_form_regist')
+            .animate({opacity: 0, top: '45%'}, 100,  // плавно меняем прозрачность на 0 и одновременно двигаем окно вверх
+                function(){ // после анимации
+                    $(this).css('display', 'none'); // делаем ему display: none;
+                    $('#overlay').fadeOut(300); // скрываем подложку
+                }
+            );
+    });
+    
+    alignCenter($('#modal_form_regist'));
+    function alignCenter(elem) {
+    elem.css({
+      left: ($(window).width() - elem.width()) / 2 + 'px', // получаем координату центра по ширине
+      top: ($(window).height() - elem.height()) / 2 + 'px' // получаем координату центра по высоте
+    })
+  }
+});
+
+// to enter
+
+$(document).ready(function() { 
+    $('a#go_enter').click( function(event){ 
+        event.preventDefault(); 
+        $('#overlay').fadeIn(400, 
+            function(){ 
+                $('#modal_form_enter') 
+                    .css('display', 'block') // 
+                    .animate({opacity: 1, top: '10%'}, 100); // плавно прибавляем прозрачность одновременно со съезжанием вниз
+        });
+    });
+    /* Закрытие модального окна, тут делаем то же самое но в обратном порядке */
+    $('#modal_close, #overlay,.send, #go_enter').click( function(){ // ловим клик по крестику или подложке
+        $('#modal_form_regist')
+            .animate({opacity: 0, top: '45%'}, 100,  // плавно меняем прозрачность на 0 и одновременно двигаем окно вверх
+                function(){ // после анимации
+                    $(this).css('display', 'none'); // делаем ему display: none;
+                    // $('#overlay').fadeOut(300); // скрываем подложку
+                }
+            );
+    });
+    
+    alignCenter($('#modal_form_enter'));
+    function alignCenter(elem) {
+    elem.css({
+      left: ($(window).width() - elem.width()) / 2 + 'px', // получаем координату центра по ширине
+      top: ($(window).height() - elem.height()) / 2 + 'px' // получаем координату центра по высоте
+    })
+  }
+});
